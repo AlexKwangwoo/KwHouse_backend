b9d00073f8f33d04f33ae591fbfdbcf2886c9e3f
Kwangwoo Alex <bnc3049@gmail.com>
2024-02-04 13:52:35 -0700

    User 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -134,6 +134,27 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 //   });
 // };
 
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead'
+  });
+};
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead'
+  });
+};
+
 exports.getUser = factory.getOne(User);
 exports.getAllUsers = factory.getAll(User);
 
